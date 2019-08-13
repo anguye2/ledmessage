0c799d3a08308419bdcdaf1563076bd9fb36dee6
An Nguyen <anguye2@pdx.edu>
2019-08-13 16:18:08 -0700

    loop messages

--- rasp_pi.py
+++ rasp_pi.py
@@ -3,24 +3,29 @@
 from samplebase import SampleBase
 from rgbmatrix import graphics
 import time
+import itertools
 import threading
 from pymongo import MongoClient
 client = MongoClient('mongodb://35.247.88.248:27017');
 
 
 message_list = []
+color_list = []
 
 def get_data():
     threading.Timer(5.0, get_data).start()
     message_list.clear()
+    color_list.clear()
     db = client.messagedata
     messages = db.messages
     all_messages = messages.find()
-
+    
     for x in all_messages:
         x = (x["name"])
         single_message = (x["message"])
+        color = (x["color"])
         message_list.append(single_message);
+        color_list.append(color);
 
 
 class RunText(SampleBase):
@@ -39,19 +44,35 @@ class RunText(SampleBase):
 
         runner = True
 
-        for i in message_list:
-            print(i)
-            while runner:
-                offscreen_canvas.Clear()
-                len = graphics.DrawText(offscreen_canvas, font, pos, 20, textColor, i)
-                pos -= 1
-                if (pos + len < 0):
-                    pos = offscreen_canvas.width
-                    runner = False 
+        while runner:
+            for (i,j) in zip(message_list,color_list):
+                color = j
+                print(color)
+                if color == 'rgb(240, 125, 125)':
+                    textColor = graphics.Color(240,125,125)
+                elif color == 'rgb(125, 192, 240)':
+                    textColor = graphics.Color(125,192,240)
+                elif color == 'rgb(240, 221, 125)':
+                    textColor = graphics.Color(240,221,125)
+                elif color == 'rgb(125, 240, 144)':
+                    textColor = graphics.Color(125,240,144)
+                elif color == 'rgb(198, 125, 240)':
+                    textColor = graphics.Color(198,125,240)
+                elif color == 'rgb(240, 125, 161)':
+                    textColor = graphics.Color(240,125,161)
+                elif color == 'rgb(255, 255, 255)':
+                    textColor = graphics.Color(255, 255, 255)
+                while runner:
+                    offscreen_canvas.Clear()
+                    len = graphics.DrawText(offscreen_canvas, font, pos, 20, textColor, i)
+                    pos -= 1
+                    if (pos + len < 0):
+                        pos = offscreen_canvas.width
+                        runner = False 
 
-                time.sleep(0.05)
-                offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
-            runner = True
+                    time.sleep(0.05)
+                    offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+                runner = True
 
 
 # Main function
